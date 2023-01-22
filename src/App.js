ee25f812a3a497c881d9fc4b758bf626f122cfd7
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-22 23:03:40 +0500

    Todolist component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Navbar from './MyComponents/Navbar';
+import TodoList from './MyComponents/TodoList';
 
 function App() {
-
-
-
   return (
     <>
-      <Navbar />
+      <Navbar/>
+      <TodoList/>
     </>
   );
 }
