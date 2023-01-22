e8a72005eb8ada761aa4b9253e4a1424eae82e8a
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-22 15:20:33 +0500

    Navbar created with design

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <>
+            <nav className=" mt-3 navbar container bg-danger navbar-light">
+                <div className="container">
+                    <a className="navbar-brand"><h3>Todos List</h3></a>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-dark" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </>
+    )
 }
